5d597cf83107c08c96faabcbe404baed9ca8d0ac
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2014-12-24 17:22:59 +0100

    feat(hire-me): add component

---
+++ public/components/hire-me/_build/hire-me.routes.js
@@ -0,0 +1,18 @@
+(function () {
+
+  angular.module('app')
+    .config(['$stateProvider', function ($stateProvider) {
+
+      $stateProvider
+        .state('hire-me', {
+          url: 'hire-me',
+          views: {
+            'content@': {
+              templateUrl: '/components/hire-me/index.html'
+            }
+          }
+        });
+
+    }]);
+
+})();
