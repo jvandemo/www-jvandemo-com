a52ad561fefd0568b560dcc6d3f4451256e6aa83
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2014-12-24 17:33:29 +0100

    feat(tools): add component

---
+++ public/components/tools/_build/tools.routes.js
@@ -0,0 +1,18 @@
+(function () {
+
+  angular.module('app')
+    .config(['$stateProvider', function ($stateProvider) {
+
+      $stateProvider
+        .state('tools', {
+          url: 'tools',
+          views: {
+            'content@': {
+              templateUrl: '/components/tools/index.html'
+            }
+          }
+        });
+
+    }]);
+
+})();
